6fdbbf141bd34dc458f6ee064788f9bc0d7a34af
gidskql6671 <gidskql6671@gmail.com>
2022-07-03 17:21:36 +0900

    Store 클래스의 필드를 기획과 같게 맞춤

--- app/src/main/java/gdsc/knu/model/Store.kt
+++ app/src/main/java/gdsc/knu/model/Store.kt
@@ -4,10 +4,10 @@ data class Store(
     val id: Int,
     val name: String,
     val description: String,
+    val tel: String,
     val address: String,
     val latitude: Double,
     val longitude: Double,
-    val representMenu: Array<String>,
-    val category: String,
-    val openingTime: String
+    val menu: Array<String>,
+    val category: String
 )
