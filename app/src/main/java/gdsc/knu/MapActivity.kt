0beee311427e2462d7fcaee0016aa7a00bc0b35e
gidskql6671 <gidskql6671@gmail.com>
2022-07-03 17:02:44 +0900

    사용자의 현재 위치를 지도에 표시

--- app/src/main/java/gdsc/knu/MapActivity.kt
+++ app/src/main/java/gdsc/knu/MapActivity.kt
@@ -3,11 +3,7 @@ package gdsc.knu
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
 import androidx.annotation.UiThread
-import androidx.core.content.ContentProviderCompat.requireContext
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.geometry.LatLngBounds
 import com.naver.maps.map.*
@@ -18,11 +14,14 @@ import gdsc.knu.databinding.ActivityMapBinding
 
 class MapActivity : AppCompatActivity(), OnMapReadyCallback {
     private val binding by lazy { ActivityMapBinding.inflate(layoutInflater) }
+    private lateinit var locationSource: FusedLocationSource
+    private lateinit var naverMap: NaverMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        locationSource = FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE)
 
         val fm = supportFragmentManager
         val mapFragment = fm.findFragmentById(R.id.map_fragment) as MapFragment?
@@ -32,16 +31,33 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions,
+                grantResults)) {
+            if (!locationSource.isActivated) {
+                naverMap.locationTrackingMode = LocationTrackingMode.None
+            }
+            return
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
     @UiThread
     override fun onMapReady(naverMap: NaverMap) {
+        this.naverMap = naverMap
+        naverMap.locationSource = locationSource
+        naverMap.locationOverlay.isVisible = true
+
+        setupMapUi(naverMap.uiSettings)
+
         naverMap.extent = LatLngBounds(
             LatLng(35.885, 128.6),
             LatLng(35.895, 128.62)
         )
-
-        naverMap.setOnMapClickListener { point, coord ->
-            println("${coord.latitude}, ${coord.longitude}")
-        }
 
         getStores().forEach { store ->
             Marker().apply {
@@ -51,9 +67,17 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
                 captionHaloColor = Color.WHITE
                 captionTextSize = 15f
             }.setOnClickListener {
-                println(store)
+                println(store.id)
                 true
             }
         }
+    }
+
+    private fun setupMapUi(uiSettings: UiSettings) {
+        uiSettings.isLocationButtonEnabled = true
+    }
+
+    companion object {
+        private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
