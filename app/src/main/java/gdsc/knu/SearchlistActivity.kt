19a2421b215872a36d098e904208c1bf4a002ef9
gidskql6671 <gidskql6671@gmail.com>
2022-07-30 16:05:27 +0900

    코루틴을 사용하여 API 호출을 하도록 수정

--- app/src/main/java/gdsc/knu/SearchlistActivity.kt
+++ app/src/main/java/gdsc/knu/SearchlistActivity.kt
@@ -1,9 +1,7 @@
 package gdsc.knu
 
 import android.content.ContentValues.TAG
-import android.os.Build
 import android.os.Bundle
-import android.os.StrictMode
 import android.text.Editable
 import android.util.Log
 import android.view.KeyEvent
@@ -13,7 +11,10 @@ import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import gdsc.knu.api.searchRestaurants
 import gdsc.knu.databinding.ActivitySearchlistBinding
-import gdsc.knu.model.SearchItem
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
 
 class SearchlistActivity : AppCompatActivity() {
     private val binding by lazy { ActivitySearchlistBinding.inflate(layoutInflater) }
@@ -22,26 +23,20 @@ class SearchlistActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        if (Build.VERSION.SDK_INT > 9) {
-            val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-            StrictMode.setThreadPolicy(policy)
-        }
-
         intent.getStringExtra("search_item")?.let { Log.e(TAG, it) }
-        val search_item = intent.getStringExtra("search_item").toString()
-        val list = dataAdd(search_item)
-        val adapter = SearchAdapter(list)
-        binding.searchRecycler.adapter=adapter
+        val searchItem = intent.getStringExtra("search_item").toString()
+
+        loadRestaurants(searchItem)
+
         binding.searchRecycler.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
 
-        binding.search.text = Editable.Factory.getInstance().newEditable(search_item)
+        binding.search.text = Editable.Factory.getInstance().newEditable(searchItem)
         binding.search.setOnKeyListener {view, keyCode, event ->
             val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
             if (event.action == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
                 imm.hideSoftInputFromWindow(binding.search.windowToken, 0)
 
-                val searchResult = dataAdd(binding.search.text.toString())
-                binding.searchRecycler.adapter = SearchAdapter(searchResult)
+                loadRestaurants(binding.search.text.toString())
 
                 true
             }
@@ -51,20 +46,17 @@ class SearchlistActivity : AppCompatActivity() {
         }
     }
 
-    // 처음에 메인 화면에서 검색했던 값의 결과를 보여줌
-    fun dataAdd(search_item : String) : List<SearchItem> {
-        var list = emptyList<SearchItem>()
-        try {
-            list = getData(search_item)
-        } catch(e: Exception){
-            e.printStackTrace()
+    private fun loadRestaurants(search_keyword : String) {
+        CoroutineScope(Dispatchers.Main).launch {
+            val response =
+                CoroutineScope(Dispatchers.IO).async {
+                    return@async searchRestaurants(search_keyword)
+                }
+
+            val searchItems = response.await()
+
+            binding.searchRecycler.adapter = SearchAdapter(searchItems)
         }
-
-        return list
-    }
-
-    fun getData(search_item: String) : List<SearchItem> {
-        return searchRestaurants(search_item)
     }
 
 }
